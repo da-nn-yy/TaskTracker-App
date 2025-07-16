a28a5b7a534f7505382b0a19ae86bb487505de76
da-nn-yy <db7394928@gmail.com>
2025-07-16 15:56:32 +0300

    Add React Router routing configuration with basic page structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import Header from "./Components/Header.jsx";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from "./Pages/login.jsx";
 function App() {
 
   return (
     <>
-      <div>
+      <Router>
         <Header />
-      </div>
+        <Routes>
+          <Route path="/" element={<h1>Home Page</h1>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/addtask" element={<h1>Add Task Page</h1>} />
+          <Route path="*" element={<h1>404 Not Found</h1>} />
+        </Routes>
+      </Router>
     </>
   )
 }
