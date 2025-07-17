7c5e2e02be845c964dd32c0342f5890cc1b0ee64
da-nn-yy <db7394928@gmail.com>
2025-07-17 22:38:00 +0300

    Add Tasks page route and import to App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./Components/Header.jsx";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from "./Pages/login.jsx";
 import Dashboard from "./Pages/main/Dashboard.jsx";
+import Tasks from "./Pages/Tasks.jsx";
 function App() {
 
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Dashboard />} />
           <Route path="/login" element={<Login/>} />
           <Route path="/addtask" element={<h1>Add Task Page</h1>} />
+          <Route path="/tasks" element={<Tasks />} />
           <Route path="*" element={<h1>404 Not Found</h1>} />
         </Routes>
       </Router>
