f23461bab458ee1d66cc8c9818b82694128591ba
da-nn-yy <db7394928@gmail.com>
2025-07-18 10:39:46 +0300

    Add Google authentication integration and animated background to Login page

--- src/Pages/login.jsx
+++ src/Pages/login.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
+import {auth,provider} from "../firebase.js"
 
 const Login = () => {
+
+  const navigate = useNavigate();
+
+  const signInWithGoogle =async  () => {
+    const result = await signInWithGoogle(auth,provider);
+    console.log(result);
+    navigate("/")
+  }
   return (
-    <div className="min-h-screen flex items-center justify-center bg-[#eaf4f4] px-2 container">
-      <div className="bg-black rounded-2xl shadow-lg p-6 sm:p-10 flex flex-col items-center w-full max-w-xs sm:max-w-md">
+    <div className="min-h-screen flex items-center justify-center bg-[#eaf4f4] px-2">
+      <div>
+        <div className="bg-[#aff901] h-70 w-70 rounded-full absolute z-1 blur-[90px] animate-pulse max-lg:hidden top-20 left-0"/>
+        <div className="bg-[#aff901] h-70 w-70 rounded-full absolute z-1 blur-[90px] animate-pulse max-lg:hidden top-60 -right-[150px]"/>
+        <div className="bg-[#aff901] h-70 w-70 rounded-full absolute z-1 blur-[90px] animate-pulse max-lg:hidden -bottom-50 left-[300px]"/>
+
+      </div>
+      <div className="bg-black rounded-2xl shadow-lg p-6 sm:p-10 flex flex-col items-center w-full max-w-xs sm:max-w-md z-10">
         <h2 className="text-2xl sm:text-3xl font-bold text-[#aff901] mb-6">Sign In</h2>
         <form className="w-full flex flex-col gap-4 sm:gap-5">
           <input
@@ -20,13 +35,20 @@ const Login = () => {
             disabled
           />
           <Link
-            className="w-full py-2 sm:py-3 rounded-full bg-[#aff901] text-black font-bold text-base sm:text-lg hover:opacity-90 transition"
+            className="w-full py-2 sm:py-3 rounded-full bg-[#aff901] text-black font-bold text-base sm:text-lg hover:opacity-90 transition duration-200"
             to={"/"}
           >
             Sign in
           </Link>
         </form>
-        <p className="text-white mt-6 text-xs sm:text-sm">Demo only. No authentication implemented.</p>
+        <div className="mt-4 w-full">
+          <button
+            onClick={signInWithGoogle}
+            className="w-full rounded-full border-[#aff901] border-2 py-2 sm:py-3 text-[#aff901] font-semibold sm:text-lg hover:opacity-90 transition duration-200">
+            Sign in wit Google
+          </button>
+
+        </div>
       </div>
     </div>
   );
