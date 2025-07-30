ef1a8aea01892850ed56774bc9998cabef6f7386
da-nn-yy <db7394928@gmail.com>
2025-07-30 12:37:57 +0300

    change the path of the file to src

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+import express from'express';
+
+import dotenv from 'dotenv'
+import taskRoutes from './routes/taskRoutes.js';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.use('/tasks', taskRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
