687f8cbecfd5dcfebe9a869c98600e01e0312d83
da-nn-yy <db7394928@gmail.com>
2025-07-30 15:28:09 +0300

    Remove commented port configuration from database connection

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,7 +5,6 @@ dotenv.config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  // port: process.env.DB_PORT || 3306,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
 });
