8b3fe0eb4ce1b69b12d669c5b799239ad3f6d3b1
da-nn-yy <db7394928@gmail.com>
2025-08-15 10:42:46 +0300

    Add database connection pool configuration and update IDE workspace

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -7,7 +7,10 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.RLWY_DB_PORT
+  port: process.env.RLWY_DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 export default pool;
